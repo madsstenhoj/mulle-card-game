be1f593c2072e126801c1e25f278ab7baf59dc89
Mads Stenhøj Andresen <mads@uniwise.dk>
2020-04-11 16:03:23 +0200

    Updates

---
+++ precache-manifest.3212bcd63ee27bd0804d38b013146e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae42f050a03342062b79987ae603262",
+    "url": "/madsstenhoj/mulle-card-game/index.html"
+  },
+  {
+    "revision": "d99135c2dc6dcac545e6",
+    "url": "/madsstenhoj/mulle-card-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "303d609a8c57ca8ba55d",
+    "url": "/madsstenhoj/mulle-card-game/static/js/2.cc908b8e.chunk.js"
+  },
+  {
+    "revision": "a97142ef21ff72a1cdac64c26781b0f1",
+    "url": "/madsstenhoj/mulle-card-game/static/js/2.cc908b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99135c2dc6dcac545e6",
+    "url": "/madsstenhoj/mulle-card-game/static/js/main.c531a567.chunk.js"
+  },
+  {
+    "revision": "10a89836ea09c58260ce",
+    "url": "/madsstenhoj/mulle-card-game/static/js/runtime-main.c90b19aa.js"
+  }
+]);
